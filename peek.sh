5145097b4e235c27a35fbf1d86dfc0dbd4c0d97c
Marta Diez Martinez <martadiezmartinez@Air-de-Marta-5.home>
2023-11-02 13:09:32 +0100

    updated 02/11/2023

--- peek.sh
+++ peek.sh
@@ -1,3 +1,9 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+if [[ -n $2 ]]; then numberlines=$2; fi
+if [[ -z $2 ]]; then numberlines=3; fi 
+
+
+if [[ $((2 * numberlines)) -ge $(wc -l < $1) ]]; then cat $1; fi
+
+
+if [[ $((2 * numberlines)) -lt $(wc -l < $1) ]]; then echo WARNING!; head -n $numberlines $1; echo ...; tail -n $numberlines $1; fi
+
