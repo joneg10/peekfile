3721474940e2a11633a438f9a331a05c5766d15c
Marta Diez Martinez <martadiezmartinez@Air-de-Marta-5.home>
2023-11-16 16:23:41 +0100

    All done except nt/aa 16/11 16:23

--- fastacan.sh
+++ fastacan.sh
@@ -9,21 +9,25 @@ if [[ -z $N ]]; then N=0;
 ### As the second input should be the number of lines, if the argument is not numerical
 ### it will print a warning message and use the default N.
 # elif [[ $N != [0-9] ]]; then echo ERROR: Non numerical value, using default value 0.;
-fi ### mejorar!
+fi
 
 ## How many files.
-if [[ $(find $directory -type f -name "*.fa" -o -name "*.fasta"  | wc -l ) -eq 0 ]]; then echo The directory is empty.;
-elif [[ $(find $directory -type f -name "*.fa" -o -name "*.fasta" | wc -l) -eq 1 ]]; then echo The directory has 1 file.;
-else echo The directory has $(find $directory -type f -name "*.fa" -o -name "*.fasta" | wc -l) files.; fi
+if [[ $(find $directory -type f -name "*.fa" -o -name "*.fasta"  | wc -l ) -eq 0 ]]; then echo The directory has no fasta files.;
+elif [[ $(find $directory -type f -name "*.fa" -o -name "*.fasta" | wc -l) -eq 1 ]]; then echo The directory has 1 fasta file.;
+else echo The directory has $(find $directory -type f -name "*.fa" -o -name "*.fasta" | wc -l) fasta files.; fi
 
-## How many unique fasta IDs.
+fastaID=0
+fastaID=$(($fastaID+$(awk -F' ' '/>/{print $1}' $directory/* | sort | uniq | wc -l)))
 
+echo There are $fastaID unique fasta IDs.
 
 ## Files:
 ### Header: filename, symlink?, sequences, total seq length (no gaps), extra: aa/nt seq?
 #### We iterate in each file of the directory.
+
 for f in $(find $directory -type f -name "*.fa" -o -name "*.fasta"); do
 
+## How many unique fasta IDs.
 #### start of the header
 header="==="
 
@@ -37,15 +41,25 @@ if [[ -h $f ]]; then header=$header" ""symlink"; fi
 header=$header" "$(grep -c ">" < $f)
 
 #### sequence length without gaps
-
+n=0
+header=$header" "$(awk '!/>/{gsub(/-/, "", $0); n=n+length($0)}END{print n}' $f)
 
 #### Echo the header before the content.
 echo $header
 
 ### Content: If fine <= 2N; full cont, if not N head, ... and N tail. If 0 skip.
+
 if [[ $N -eq 0 ]]; then continue;
-elif [[ $((2 * $N)) -ge $LENGTH ]]; then cat $SECUENCIA;
-elif [[ $((2 * $N)) -le $LENGTH ]]; then head -n $N $SECUENCIA; echo ...; tail -n $N $SECUENCIA; fi
+
+elif [[ $((2 * $N)) -gt $(wc -l < $f) ]];
+then cat $f;
+
+
+elif [[ $((2 * $N)) -le $(wc -l < $f) ]];
+then head -n $N $f;
+echo ...;
+tail -n $N $f;
+fi
 
 done
 
