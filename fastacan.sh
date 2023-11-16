a17cfd49db1987562d5d56b7ded8f651766adfd3
Marta Diez Martinez <martadiezmartinez@Air-de-Marta-5.home>
2023-11-16 11:54:50 +0100

    Args+lines 16/11 11:54

--- fastacan.sh
+++ fastacan.sh
@@ -1,14 +1,22 @@
 # MIDTERM ASSIGNMENT
 ## Prepare the optional arguments
-directory=$2 # Folder
-N=$3 # Number of lines
+directory=$1 # Folder
+N=$2 # Number of lines
 
 ## Define the default arguments.
 if [[ -z $directory ]]; then directory='.'; fi
-if [[ -z $N ]]; then N=0; fi
+if [[ -z $N ]]; then N=0;
+### As the second input should be the number of lines, if the argument is not numerical
+### it will print a warning message and use the default N.
+# elif [[ $N != [0-9] ]]; then echo ERROR: Non numerical value, using default value 0.;
+fi
+
 
 ## How many files.
-
+if [[ $(ls $directory | wc -l) -eq 0 ]]; then echo The directory is empty.;
+elif [[ $(ls $directory | wc -l) -eq 1 ]]; then echo The directory has 1 file.;
+else echo The directory has $(ls $directory | wc -l) files.; fi
+echo $directory $N
 
 ## How many unique fasta IDs.
 
