dd9d9e83a9736c65fa825b15694f29572c9aa732
Marta Diez Martinez <martadiezmartinez@Air-de-Marta-5.home>
2023-11-20 09:07:47 +0100

    Problem solving& clear comments, remaining: clean headers¿?, non interger input, non-readable files¿? 20/11 09:06

--- fastacan.sh
+++ fastacan.sh
@@ -19,24 +19,23 @@ else echo The directory has $(find $directory -type f -name "*.fa" -o -type f -n
 ## How many unique fasta IDs.
 fastaID=0
 for file in $(find $directory -type f -name "*.fa" -o -name "*.fasta"); do
-fastaID=$(($fastaID+$(awk -F' ' '/>/{print $1}' $file | sort | uniq | wc -l))2>fastacan.err)
+fastaID=$(($fastaID+$(awk -F' ' '/>/{print $1}' $file | sort | uniq | wc -l))2>fastacan.err) # We redirect this error to a file because we want a clean output, most of times will be because a file is not readable, we'll let the user know later.
 done
-
 
 echo There are $fastaID unique fasta IDs.
 
 
 ## Files:
-### Header: filename, symlink?, sequences, total seq length (no gaps), extra: aa/nt seq?
-#### We iterate in each file of the directory.
+### Header: filename, symlink?, sequences, total seq length (no gaps), extra: aa/nt seq.
 
+#### We iterate in each file of the directory.
 for f in $(find $directory -type f -name "*.fa" -o -name "*.fasta"); do
 
-if [[ ! -r $f ]]; then echo === $(basename $f) not readable file! && continue; fi
- # We used basename command in PGB that's why I know it!
+##### We'll let the user know if a file is not readable, just in case.
+if [[ ! -r $f ]]; then echo === $(basename $f) not readable file! && continue; fi #We used basename command in PGB that's why I know it!
 
 
-#### start of the header
+#### Creation of the header
 header="==="
 
 #### filename
@@ -45,7 +44,7 @@ header=$header" "$(basename $f)
 #### is it a symlink? 
 if [[ -h $f ]]; then header=$header" ""symlink"; fi
 
-#### sequences
+#### sequences: by counting how many fasta headers there are.
 header=$header" "$(grep -c ">" < $f)"seqs"
 
 #### sequence length without gaps
@@ -53,20 +52,19 @@ n=0
 header=$header" "$(awk '!/>/{gsub(/-/, "", $0); n=n+length($0)}END{print n}' $f)
 
 
-#### amino acid sequence or nucleotide sequence?
+#### amino acid sequence or nucleotide sequence? If the sequence hast any character that is not a nucleotide (A, C, T, G) then it will be determined as an aminoacidic sequence.
 if [[ $(awk '!/>/{gsub(/-/, "", $0); print $0}' $f | grep -vi ^[A,C,T,G]) ]]; then header=$header" aminoacid_sequence"; else header=$header" nucleotide_sequence"; fi
 
 
 #### Echo the header before the content.
 echo $header
 
-### Content: If fine <= 2N; full cont, if not N head, ... and N tail. If 0 skip.
+### Content: If line <= 2N; full content, if not N head, ... and N tail. If 0 skip.
 
 if [[ $N -eq 0 ]]; then continue;
 
 elif [[ $((2 * $N)) -gt $(wc -l < $f) ]];
 then cat $f;
-
 
 elif [[ $((2 * $N)) -le $(wc -l < $f) ]];
 then head -n $N $f;
@@ -76,4 +74,3 @@ echo ; # This echo is just to make a line jump between different files.
 fi
 
 done
-
