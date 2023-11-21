d523a15510735ab3678562d6c5a53c0152d11dcb
Marta Diez Martinez <martadiezmartinez@Air-de-Marta-5.home>
2023-11-21 10:53:34 +0100

    JUST COMMENTING LEFT

--- fastacan.sh
+++ fastacan.sh
@@ -5,11 +5,7 @@ N=$2 # Number of lines
 
 ## Define the default arguments.
 if [[ -z $directory ]]; then directory='.'; fi
-if [[ -z $N ]]; then N=0;
-### As the second input should be the number of lines, if the argument is not numerical
-### it will print a warning message and use the default N.
-# elif [[ $N != [0-9] ]]; then echo ERROR: Non numerical value, using default value 0.;
-fi
+if [[ -z $N ]]; then N=0; fi
 
 ## How many files.
 if [[ $(find $directory -type f -name "*.fa" -o -type f -name "*.fasta"  | wc -l ) -eq 0 ]]; then echo The directory has no fasta files.;
@@ -24,11 +20,11 @@ done
 
 echo There are $fastaID unique fasta IDs.
 
-
 ## Files:
 ### Header: filename, symlink?, sequences, total seq length (no gaps), extra: aa/nt seq.
 
 #### We iterate in each file of the directory.
+
 for f in $(find $directory -type f -name "*.fa" -o -name "*.fasta"); do
 
 ##### We'll let the user know if a file is not readable, just in case.
@@ -39,7 +35,6 @@ header="==="
 
 #### filename
 header=$header" "$f
-# EL PATHWAY!¡
 
 #### is it a symlink? 
 if [[ -h $f ]]; then header=$header" ""symlink"; fi
@@ -50,7 +45,6 @@ header=$header" Sequences:"$(grep -c ">" < $f)
 #### sequence length without gaps
 n=0
 header=$header" Length:"$(awk '!/>/{gsub(/-/, "", $0);n=n+length($0)}END{print n}' $f)
-
 
 #### amino acid sequence or nucleotide sequence? If the sequence hast any character that is not a nucleotide (A, C, T, G) then it will be determined as an aminoacidic sequence.
 if [[ $(awk '!/>/{gsub(/-/, "", $0); print $0}' $f | grep -vi ^[A,C,T,G]) ]]; then header=$header" aminoacid_sequence"; else header=$header" nucleotide_sequence"; fi
